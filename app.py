402672eacbdf12134c941fd55fe830eb5e838a9b
cieaseden-blip <cieaseden@gmail.com>
2026-07-10 00:28:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from huggingface_hub import InferenceClient
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 # Optimizamos a Gemma 2 9B (puedes cambiarlo si prefieres mantener Llama 3)
-MODELO_ACTIVO = "google/gemma-2-9b-it"
+MODELO_ACTIVO = "Qwen/Qwen2.5-72B-Instruct"
 
 # Inicializar el cliente de inferencia
 client = InferenceClient(MODELO_ACTIVO, token=HF_TOKEN)
